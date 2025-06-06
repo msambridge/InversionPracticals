66d758560b6def98201b1abfc113b2ed7359ea5a
Malcolm Sambridge <44758198+msambridge@users.noreply.github.com>
2025-06-07 06:53:01 +1000

    updated earthquake notebooks

--- software/eqlocate.py
+++ software/eqlocate.py
@@ -34,48 +34,6 @@ def readborderdata(filename):
 
     return np.array(border1),np.array(border2)
 
-##################
-def eqlocate_orig(x0,y0,z0,ts,la,lo,el,vp,tol,solvedep=False):
-    la2km=111.19
-    lo2km=75.82
-    
-    i=5-1
-    t0=ts[i]-np.sqrt(((x0*lo2km-lo[i]*lo2km)**2)+((y0*la2km-la[i]*la2km)**2)+(el[i]-z0)**2)/vp  # initial guess origin time
-    
-    ni=0
-    sols=[[t0,x0,y0,z0]]
-    ndata = len(ts) # Number of data
-    
-    while 1:
-        ni=ni+1
-        D0=[]
-        for i in range(ndata):
-            D0.append(np.sqrt((lo[i]*lo2km-x0*lo2km)**2+(la[i]*la2km-y0*la2km)**2+(el[i]-z0)**2))
-        G=[]
-        d=[]
-        for i in range(ndata):
-            if(solvedep):
-                G.append([1,((x0-lo[i])*lo2km)/(D0[i]*vp),((y0-la[i])*la2km)/(D0[i]*vp),((z0-el[i]))/(D0[i]*vp)])
-            else:
-                G.append([1,((x0-lo[i])*lo2km)/(D0[i]*vp),((y0-la[i])*la2km)/(D0[i]*vp)])
-            d.append(ts[i]-(D0[i]/vp+t0))
-        G=np.array(G)
-        d=np.array(d)
-        m=np.linalg.lstsq(G,d)[0]
-        t0=t0+m[0]
-        x0=x0+m[1]/lo2km
-        y0=y0+m[2]/la2km
-        if(solvedep): 
-            z0=z0+m[3]
-            dtol = np.sqrt(m[1]**2+m[2]**2+m[3]**2) # distance moved by hypocentre
-        else:
-            dtol = np.sqrt(m[1]**2+m[2]**2)
-        sols.append([t0,x0,y0,z0])        
-        if m[0]<tol[0] and dtol<tol[1]:
-            break
-    sols=np.array(sols)
-    return sols, d
-
 def eqlocate(x0,y0,z0,ts,la,lo,el,vpin,tol,solvedep=False,nimax=100,verbose=False,kms2deg=[111.19,75.82]):
     la2km=kms2deg[0]
     lo2km=kms2deg[1]
@@ -127,27 +85,34 @@ def eqlocate(x0,y0,z0,ts,la,lo,el,vpin,tol,solvedep=False,nimax=100,verbose=Fals
     sols=np.array(sols)
     return sols, res
 
-def plot_eq_solutions(MCsols,sols,title="Monte Carlo solutions"):
+def plot_eq_solutions(MCsols,sols,title="Monte Carlo solutions",plotdepth=False):
     fig = plt.figure(figsize=(9,6))
     fig.suptitle(title, fontsize=20)
 
     ax1 = plt.subplot(221)
-    ax1.plot(MCsols[:,1],MCsols[:,2], 'k+')
+    ax1.plot(MCsols[:,1],MCsols[:,2], 'k+',lw=0.5)
     ax1.plot(sols[-1,1],sols[-1,2], 'ro')
     ax1.set_xlabel('Longitude')
     ax1.set_ylabel('Latitude')
 
     ax2 = plt.subplot(222)
-    ax2.plot(MCsols[:,1],MCsols[:,0], 'k+')
+    ax2.plot(MCsols[:,1],MCsols[:,0], 'k+',lw=0.5)
     ax2.plot(sols[-1,1],sols[-1,0], 'ro')
     ax2.set_xlabel('Longitude')
     ax2.set_ylabel('Origin Time')
 
     ax3 = plt.subplot(223)
-    ax3.plot(MCsols[:,2],MCsols[:,0], 'k+')
+    ax3.plot(MCsols[:,2],MCsols[:,0], 'k+',lw=0.5)
     ax3.plot(sols[-1,2],sols[-1,0], 'ro')
     ax3.set_xlabel('Latitude')
     ax3.set_ylabel('Origin Time')
+
+    if(plotdepth):
+        ax4 = plt.subplot(224)
+        ax4.plot(MCsols[:,3],MCsols[:,0], 'k+',lw=0.5)
+        ax4.plot(sols[-1,3],sols[-1,0], 'ro')
+        ax4.set_xlabel('Depth')
+        ax4.set_ylabel('Origin Time')
 
     plt.show()
 
